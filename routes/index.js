a7aba0b1d8980371430df24846a5e5f252736218
Byron Chu <chu.byron@yahoo.com>
2016-12-13 12:22:52 +1100

    spotify api completed also index.js adjusted

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
+
+// cont {searchArtists, searchTracks } = require('../lib/spotify-api')
+const spotifyAPI = require('../lib/spotify-api')
+
+
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/artists/:name', function(req, res, next) {
+  const { name } = req.params;
+  spotifyAPI.searchArtists(name)
+  .then((artists) => {
+    res.json(artists);
+  })
+  .catch((error) => {
+    next(error);
+  });
+//  res.render('index', { title: 'Express' });
+});
+
+
+
+router.get('/', function(req, res, next) {
+  res.render('/tracks/:name', { title: 'Express' });
+});
+
+
+
+
 
 module.exports = router;
